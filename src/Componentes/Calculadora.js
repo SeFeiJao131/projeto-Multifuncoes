e154a9d64d12290acdbd1b8d140d41c007f55f4a
Rafael Mizobuchi <rafaelmizob@gmail.com>
2026-04-15 13:24:32 -0300

    fix: estrutura correta

--- src/Componentes/Calculadora.js
+++ src/Componentes/Calculadora.js
@@ -3,6 +3,14 @@ import { useState } from "react";
 function Calculadora() {
   const [valor, setValor] = useState("");
 
+  const calcular = () => {
+    try {
+      setValor(eval(valor));
+    } catch {
+      setValor("Erro");
+    }
+  };
+
   return (
     <div>
       <h2>Calculadora</h2>
@@ -11,7 +19,7 @@ function Calculadora() {
         <button onClick={() => setValor(valor + "1")}>1</button>
         <button onClick={() => setValor(valor + "2")}>2</button>
         <button onClick={() => setValor(valor + "+")}>+</button>
-        <button onClick={() => setValor(eval(valor))}>=</button>
+        <button onClick={calcular}>=</button>
       </div>
     </div>
   );
